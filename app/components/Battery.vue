6ab115f4bd802c602df94c6178c7df8539cca480
endusol <pdima.endusol@gmail.com>
2024-04-23 20:37:50 +0300

    Fixed coloring in the battery component. Added templating to the battery component (bound to the 'indicator' attr).

--- app/components/Battery.vue
+++ app/components/Battery.vue
@@ -1,13 +1,14 @@
 <script setup>
 const props = defineProps([ 'indicator' ])
+
+if (props.indicator.length > 4 || props.indicator.length < 0) {
+    throw Error('Invalid indicator length. Must be between 0 and 4.')
+}
 </script>
 
 <template>
 <div class="c c--battery">
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
+    <div v-for="_ in indicator"></div>
 </div>
 </template>
 
@@ -18,14 +19,14 @@ const props = defineProps([ 'indicator' ])
     height: 22px;
     display: grid;
     grid-template-columns: 1fr 1fr 1fr 1fr;
-    border: solid 2px var(--clr--dark-green);
+    border: solid 2px var(--clr--fnt--primary);
     border-radius: 4px;
     gap: 2px;
     padding: 2px;
     box-sizing: border-box
 }
 .c--battery > div {
-    background: var(--clr--dark-green);
+    background: var(--clr--fnt--primary);
     border-radius: 2px;
     flex: .25;
 }
@@ -36,7 +37,7 @@ const props = defineProps([ 'indicator' ])
     position: absolute;
     right: -6px;
     top: 4.5px;
-    background-color: var(--clr--dark-green);
+    background-color: var(--clr--fnt--primary);
     border-top-right-radius: 2px;
     border-bottom-right-radius: 2px;
 }
