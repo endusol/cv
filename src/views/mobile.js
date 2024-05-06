0c9ae687522240634dabd411ac9e53af0dcdc5a8
endusol <pdima.endusol@gmail.com>
2024-05-06 22:34:12 +0300

    Duo version site version in progress

---
+++ src/views/mobile.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from '../components/AppMobile.vue'
+
+
+window.app = createApp(App)
+window.app.mount('body')
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     root: './src',
     plugins: [ vue() ],
-    resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
+    // resolve: { alias: { '@': fileURLToPath(new URL('./cv', import.meta.url)) } },
     build: {
         outDir: '../.build',
         rollupOptions: {
